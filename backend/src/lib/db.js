1cd3627de0e901c977e3c991955fc0522905d9bd
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-09-29 23:02:58 +0700

    refactor code

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose"
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const { MONGO_URI } = ENV;
+        if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+
+        const conn = await mongoose.connect(MONGO_URI)
         console.log("Mongo connected", conn.connection.host)
     }
     catch (err) {
