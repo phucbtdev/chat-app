9c6b3b8107fcaa1f0ab3150baf41453eb4e46f16
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-09-29 22:01:44 +0700

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,9 @@
 import express from "express"
+import { signup } from "../controller/auth.controller.js"
 
 const router = express.Router()
 
-router.get("/register", (req, res) => {
-    res.json("register")
-})
+router.post("/signup", signup)
 
 router.get("/login", (req, res) => {
     res.json("login")
