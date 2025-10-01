2cc383cf8bcfb1f9c6dd33f513f4575fb66c8ca8
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-10-02 00:12:18 +0700

    update profile pic using cloudinary

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { signup, login, logout } from "../controller/auth.controller.js"
+import { signup, login, logout, updateProfile } from "../controller/auth.controller.js"
+import { protectedRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router()
 
 router.post("/signup", signup)
 router.post("/login", login)
 router.post("/logout", logout)
+router.put("/update-profile", protectedRoute, updateProfile)
+
 
 export default router
